c90ebd64898af0a7be5313e87307bb7dd19ec6d5
tannercurtis <tanner.curtis@nytimes.com>
2017-07-19 15:42:53 -0400

    Near Complete

--- js/project.js
+++ js/project.js
@@ -1,1 +1,17 @@
-// get the image ID and change the src after 2 seconds
+$(".grid").masonry({
+	itemSelector: '.grid-images',
+	columnWidth: ".grid-images",
+	percentPosition: true
+});
+
+$("a.gallery").featherlightGallery({
+	galleryFadeIn: 0,
+	galleryFadeOut: 0
+});
+
+$("nav a").hover(function(){
+    $("nav a").removeClass("current");
+    $(this).addClass("current");
+});
+
+//https://github.com/noelboss/featherlight/wiki/Gallery:-swipe-on-touch-devices
