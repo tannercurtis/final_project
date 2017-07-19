d71d3114ba89cfd1f97289b5658c9e8de3e1cc63
Tanner Curtis <tanner.curtis@nytimes.com>
2017-07-19 18:42:17 -0400

    Updating gh-pages from master branch via GitBot

--- js/project.js
+++ js/project.js
@@ -1,17 +1,19 @@
-$(".grid").masonry({
-	itemSelector: '.grid-images',
-	columnWidth: ".grid-images",
-	percentPosition: true
-});
+$(".grid").imagesLoaded( function(){
+	$(".grid").masonry({
+			itemSelector: '.grid-images',
+			columnWidth: ".grid-images",
+			percentPosition: true
+	});
 
-$("a.gallery").featherlightGallery({
-	galleryFadeIn: 0,
-	galleryFadeOut: 0
-});
+	$("a.gallery").featherlightGallery({
+		galleryFadeIn: 0,
+		galleryFadeOut: 0
+	});
 
-$("nav a").hover(function(){
-    $("nav a").removeClass("current");
-    $(this).addClass("current");
+	$("nav a").hover(function(){
+	    $("nav a").removeClass("current");
+	    $(this).addClass("current");
+	});
 });
 
 //https://github.com/noelboss/featherlight/wiki/Gallery:-swipe-on-touch-devices
